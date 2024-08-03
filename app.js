138ebc3472511e59a3acb7e88a79d89823327209
Nishant Raj <nishantraj2109@gmail.com>
2024-08-03 13:54:21 +0530

    removed .env

--- app.js
+++ app.js
@@ -2,14 +2,13 @@ const express = require('express');
 const app = express();
 const notesRouter = require('./src/routes/notes');
 
-require('dotenv').config()
 app.use(express.json());
 
 
 // Routes
 app.use('/notes', notesRouter);
 
-const PORT = process.env.PORT || 8001;
+const PORT = 8000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
